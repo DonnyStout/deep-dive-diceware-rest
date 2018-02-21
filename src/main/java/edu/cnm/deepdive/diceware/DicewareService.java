696367039428e9330cf20845f61ce20e25a77abe
DonnyStout <donnystout47@gmail.com>
2018-02-20 21:35:24 -0700

    changed the invocation of SecureRandom for sake of performance

--- src/main/java/edu/cnm/deepdive/diceware/DicewareService.java
+++ src/main/java/edu/cnm/deepdive/diceware/DicewareService.java
@@ -2,12 +2,18 @@ package edu.cnm.deepdive.diceware;
 
 import edu.cnm.deepdive.security.Diceware;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DicewareService implements PassphraseService {
 
-  private Diceware diceware = new Diceware();
+  private Diceware diceware;
+
+  {
+    diceware = new Diceware();
+    diceware.setRng(new SecureRandom());
+  }
 
   @Override
   public String generate(int length, String delimiter, boolean duplicatesAllowed)
